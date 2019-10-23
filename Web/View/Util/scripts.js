db47d589d4a374fd854f04e17419f69e40c210fd
heymeuquerido@gmail.com <heymeuquerido@gmail.com>
2019-10-23 15:07:38 -0300

    qualquer coisa

--- Web/View/Util/scripts.js
+++ Web/View/Util/scripts.js
@@ -1,35 +1,48 @@
-﻿function enviar() {
-    function enviar() {
-        var pacote = {}
-        pacote.nCdEmpresa = "";
-        pacote.sDsSenha = "";
-        pacote.sCepOrigem = $('input[id="sCepOrigem"]').val();
-        pacote.sCepDestino = $('input[id="sCepDestino"]').val();
-        pacote.nVlPeso = $('input[id="nVlPeso"]').val();
-        pacote.nCdFormato = $('input[id="nCdFormato"]').val();
-        pacote.nVlComprimento = $('input[id="nVlComprimento"]').val();
-        pacote.nVlAltura = $('input[id="nVlAltura"]').val();
-        pacote.nVlLargura = $('input[id="nVlLargura"]').val();
-        pacote.sCdMaoPropria = $('input[id="sCdMaoPropria"]').val();
-        pacote.nVlValorDeclarado = $('input[id="nVlValorDeclarado"]').val();
-        pacote.sCdAvisoRecebimento = $('input[id="sCdAvisoRecebimento"]').val();
-        pacote.nCdServico = $('input[id="nCdServico"]').val();
-        pacote.nVlDiametro = $('input[id="nVlDiametro"]').val();
+﻿function teste(msg) {
+    alert(msg);
+}
 
-        var url = "http://usysweb.com.br/api/correiosambev.php?";
+function enviar() {
+    var pacote = {}
+    pacote.nCdEmpresa = "";
+    pacote.sDsSenha = "";
+    pacote.sCepOrigem = $('input[id="sCepOrigem"]').val();
+    pacote.sCepDestino = $('input[id="sCepDestino"]').val();
+    pacote.nVlPeso = $('input[id="nVlPeso"]').val();
+    pacote.nCdFormato = 1;
+    pacote.nVlComprimento = $('input[id="nVlComprimento"]').val();
+    pacote.nVlAltura = $('input[id="nVlAltura"]').val();
+    pacote.nVlLargura = $('input[id="nVlLargura"]').val();
+    pacote.nVlDiametro = 0;
+    //pacote.sCdMaoPropria = $('input[id="sCdMaoPropria"]').val();
 
-        $.each(pacote, function (key, value) {
-            url += key + "=" + value + "&";
+
+    //var temp;
+    //temp = $('input[id="sCdMaoPropria"]').selectedIndex;
+    //teste(temp);
+
+
+    //pacote.sCdAvisoRecebimento = $('input[id="sCdAvisoRecebimento"]').val();
+    //pacote.nCdServico = $('input[id="nCdServico"]').val();
+    
+
+    var url = "http://usysweb.com.br/api/correiosambev.php?";
+
+    $.each(pacote, function (key, value) {
+        url += key + "=" + value + "&";
+    });
+    url += "StrRetorno=xml&nIndicaCalculo=3";
+    //aqui a url ta pronta bonitinha, aí eu vou estragar ela pra conseguir testar usando exemplo = true 
+    url = "http://usysweb.com.br/api/correiosambev.php?exemplo=true";
+
+    // aqui vem o get
+
+    $.get(url, function (data) {
+        var content = "";
+        $.each(JSON.parse(data).cServico, function (key, value) {
+            //document.getElementById(key).textContent = value;
+            content += key + ": " + value + "\n";
         });
-        url += "StrRetorno=xml&nIndicaCalculo=3";
-        //aqui a url ta pronta bonitinha, aí eu vou estragar ela pra conseguir testar usando exemplo = true 
-        url = "http://usysweb.com.br/api/correiosambev.php?exemplo=true";
-
-        // aqui vem o get
-
-        $.get(url, function (data) {
-            $.each(JSON.parse(data).cServico, function (key, value) {
-                document.getElementById(key).textContent = value;
-            });
-        });
-    }
+        alert(content);
+    });
+}
